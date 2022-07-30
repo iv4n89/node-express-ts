96f5b8680a8d42ddb2373f16a11de3f8131755de
Iván Betanzos Macías <69988988+iv4n89@users.noreply.github.com>
2022-07-30 19:38:58 +0200

    Add files via upload

--- database/db.ts
+++ database/db.ts
@@ -2,10 +2,6 @@ import { DataSource } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 /**
- * MySQL is set as default database type. To switch to mongo change the default export to mongoDataSource
- */
-
-/**
  * Main app datasource. MySQL type
  */
 const AppDataSource = new DataSource({
@@ -18,17 +14,6 @@ const AppDataSource = new DataSource({
     entities: ['models/**/*{.js,.ts}'],
     synchronize: true,
     namingStrategy: new SnakeNamingStrategy()
-});
-
-/**
- * MongoDB/DocumentDB datasource
- */
-const mongoDataSource = new DataSource({
-    type: process.env.MONGO_TYPE as 'mongodb' || 'mongodb',
-    host: process.env.MONGO_HOST,
-    port: Number(process.env.MONGO_PORT) || 27017,
-    entities: ['models/**/*{.js,.ts}'],
-    synchronize: true,
 });
 
 export default AppDataSource;
